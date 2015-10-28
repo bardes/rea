a9611274fc618b1388cdb7b6ecf59ed4e4c1df16
Paulo Bardes <paulobardes@ig.com.br>
2015-10-28 05:31:06 -0200

    Dormir eh para os fracos!

--- js/demo.js
+++ js/demo.js
@@ -1,8 +1,35 @@
 is_precise = true;
 
-$(document).ready(function(){
+function setHelp(msg) {
+    $("#help").html(msg);
+}
+
+function clearHelp() {
+    setHelp("");
+}
+
+function resetSim() {
+    // Reativa dica para os botões
+    $("#reset").hover(function(){setHelp("Reinicia a simulação.")}, clearHelp);
+    $("#advance").hover(function(){setHelp("Avança a simulação. (Um pulso de clock)")}, clearHelp);
+    $("#int-type").hover(function(){setHelp("Escolhe entre interrupções precisas ou imprecisas.")}, clearHelp);
+}
+
+$(function(){
     $("#int-type").click(function(){
         is_precise = !is_precise;
         $("#int-type").html(is_precise ? "Precisa" : "Imprecisa");
     });
+
+    $("#advance").click(function(){
+        $("#stack > tbody:last-child").prepend("<tr><td>0xFFAA</td><td>Oi</td></tr>");
+        //if(state == ativo) avanca(); else comeca();
+    });
+
+    $("#reset").click(function(){
+        $("#execute").css("background-color", "red");
+    });
+
 });
+
+
